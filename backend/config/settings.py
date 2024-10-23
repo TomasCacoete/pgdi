74e038d9fb38f66a867eebd99cd6f881805a17c8
TomasCacoete <tomaspcacoete@gmail.com>
2024-10-23 12:15:35 +0100

    pra main

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/pgdi_api/models.py
+++ backend/pgdi_api/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone_no=models.CharField(max_length=15)
-    
+
     USER_TYPES = [
         ('contestant', 'Contestant'),
         ('creator', 'Creator'),
@@ -28,29 +27,26 @@ class Creator(models.Model):
         return f"Creator: {self.user.username}"
     
     
+class Route(models.Model):
+    creator=models.ForeignKey(Creator, on_delete=models.CASCADE)    
+    file=models.FileField(upload_to='routes/')
+
+
 class Competition(models.Model):
     name=models.CharField(max_length=50)
     start_date=models.DateTimeField()
     end_date=models.DateTimeField()
     creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    routes = models.ManyToManyField(Route)
 
-class Route(models.Model):
-    creator=models.ForeignKey(Creator, on_delete=models.CASCADE)    
-    file=models.FileField(upload_to='routes/')
-    
-
-class CompetitionRoute(models.Model):
-    competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
-    route=models.ForeignKey(Route, on_delete=models.CASCADE)
-    
-    class Meta:
-        unique_together = ('competition', 'route')
+    def __str__(self):
+        return self.name
         
         
 class Submission(models.Model):
     file=models.FileField(upload_to='submissions/')
     contestant=models.ForeignKey(Contestant, on_delete=models.CASCADE)
-    competition=models.ForeignKey(Competition, on_delete=models.CASCADE) # ? a
+    competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
     score=models.FloatField()
     
     class Meta:
