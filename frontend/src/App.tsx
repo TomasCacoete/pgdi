4bc3a7be2aa3d3cdcfd1bbb8165e46912d04c046
TomasCacoete <tomaspcacoete@gmail.com>
2024-11-08 17:36:09 +0000

    limpar a main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './auth/authContext';
-import Home from './Pages/HomePage/Home.tsx';
-import Login from './Pages/LoginPage/Login';
-import MapsPage from './Pages/Maps_API_Tests/maps.tsx';
-import { APIProvider } from '@vis.gl/react-google-maps';
 
 function App()  {
   return (
     <>
       <Router>
         <AuthProvider>
-          <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
             <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/map" element={<MapsPage />} />
+              <Route path="/" element={<div>Ola</div>}/>
             </Routes>
-          </APIProvider>
         </AuthProvider>
       </Router>
     </>
