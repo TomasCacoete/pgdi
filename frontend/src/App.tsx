22e7c920f66416295e101bca531d98c60f575eec
gugax1515 <barcouco11@gmail.com>
2024-11-15 16:30:36 +0000

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './auth/authContext';
 import Home from './Pages/HomePage/Home.tsx';
 import Login from './Pages/LoginPage/Login';
-import MapsPage from './Pages/Maps_API_Tests/maps.tsx';
-import { APIProvider } from '@vis.gl/react-google-maps';
 import UPLOAD from './Pages/FileUpload/uploadFile.tsx'
 
 function App()  {
@@ -11,14 +9,11 @@ function App()  {
     <>
       <Router>
         <AuthProvider>
-          <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/map" element={<MapsPage />} />
-              <Route path="/upload" element={<UPLOAD/>} />
-            </Routes>
-          </APIProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/upload" element={<UPLOAD/>} />
+          </Routes>
         </AuthProvider>
       </Router>
     </>
